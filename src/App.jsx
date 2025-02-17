1bd2acec75a3f00b5eadf1862ef8a1e00d5587be
Donjella <brendonang@gmail.com>
2025-02-17 19:59:38 +1100

    feat: pokemonteambuilder form started

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { PokemonTeamBuilder } from './PokemonTeamBuilder'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,6 +17,7 @@ function App() {
   
   return (
     <>
+      <PokemonTeamBuilder />
       <button onClick={coolFunction} >
         Click me!
       </button>
